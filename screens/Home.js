a5a606d906fb686dc5502c3f8d6c2c055dbe73c6
Ryodai Makino <davinci0327@icloud.com>
2021-11-17 23:05:29 +0900

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+	return (
+		<SafeAreaView>
+			<HeaderTabs />
+		</SafeAreaView>
+	)
+}
