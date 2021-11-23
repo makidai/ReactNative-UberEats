c5038ec16a94f716a9efd1649308b217d040d324
Ryodai Makino <davinci0327@icloud.com>
2021-11-23 16:01:30 +0900

    connect yelp api

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItems, { localRestaurants } from "../components/RestaurantItems";
+import RestaurantItems, {
+    localRestaurants,
+} from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
 
+const YELP_API_KEY =
+    "rl91GOhPiGgQVae1-VmG4WEDSFZLVscz_ULBIwThoYkIjYGb7SdiUD3vNeeEG3N3hxGWZv3dUwntWhmKZZT4yVL_YwSDs4oIXIatQ8bRFkU_rOjeAntTW00dJI2cYXYx";
+
 export default function Home() {
-    const [restaurantData, setRestaurantData] = React.useState(localRestaurants)
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
+    const [city, setCity] = useState("Hounancho, suginami-ku, Tokyo, JP");
+    const [activeTab, setActiveTab] = useState("Delivery");
+
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+        const apiOptions = {
+            headers: {
+                Authorization: `Bearer ${YELP_API_KEY}`,
+            },
+        };
+        return fetch(yelpUrl, apiOptions)
+            .then((res) => res.json())
+            .then((json) =>
+                setRestaurantData(
+                    json.businesses.filter((business) =>
+                        business.transactions.includes(activeTab.toLowerCase())
+                    )
+                )
+            );
+    };
+
+    useEffect(() => {
+        getRestaurantsFromYelp();
+    }, [city, activeTab]);
+
     return (
         <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
             <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -17,7 +48,7 @@ export default function Home() {
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
-                <RestaurantItems restaurantData={restaurantData}/>
+                <RestaurantItems restaurantData={restaurantData} />
             </ScrollView>
         </SafeAreaView>
     );
