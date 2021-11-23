0946bae8c59ff398ade461c65ec9cadfb41f79ac
Ryodai Makino <davinci0327@icloud.com>
2021-11-23 17:36:23 +0900

    fix header tabs

--- screens/Home.js
+++ screens/Home.js
@@ -43,7 +43,7 @@ export default function Home() {
     return (
         <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
             <View style={{ backgroundColor: "white", padding: 15 }}>
-                <HeaderTabs />
+                <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
                 <SearchBar />
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
